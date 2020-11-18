38f70b09ca47cf0f8a2718cc332d4ce529f1f088
Mathura <mathuradas333@gmail.com>
2020-11-18 20:03:12 +0530

    [Mathura]Refractor. Use of functions to get emp wage and emp hours

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -4,17 +4,25 @@ const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
 
-let empCheck = Math.floor(Math.random() * 10) % 3;
-let empWage =0;
-switch(empCheck){
-    case 1:
-        empWage = PART_TIME_HOURS*WAGE_PER_HOUR;
-        break;
-    case 2:
-        empWage = FULL_TIME_HOURS*WAGE_PER_HOUR;
-        break;
-    default:
-        empWage = 0;
-        break;
+function GetWorkHours(empCheck) {
+    let workHours = 0;
+    switch (empCheck) {
+        case 1:
+            workHours = PART_TIME_HOURS;
+            break;
+        case 2:
+            workHours = FULL_TIME_HOURS;
+            break;
+        default:
+            workHours = 0;
+            break;
+    }
+    return workHours;
 }
-console.log('Employee Wage is : '+ empWage);
+function GetEmpWage(empHours){
+    return empHours*WAGE_PER_HOUR;
+}
+
+let empCheck = Math.floor(Math.random() * 10) % 3;
+let empWage = GetEmpWage(GetWorkHours(empCheck));
+console.log('Employee Wage is : ' + empWage);
