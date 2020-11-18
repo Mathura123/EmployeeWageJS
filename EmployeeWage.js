52bf5bbc5cf6b36e59b484330af175473657fd39
Mathura <mathuradas333@gmail.com>
2020-11-18 19:13:52 +0530

    [Mathura]Add. Ability to Calculate Daily Wage on basis of absent or full time  or part time

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,9 +1,20 @@
-const IS_ABSENT = 0;
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HOURS = 4;
+const FULL_TIME_HOURS = 8;
+const WAGE_PER_HOUR = 20;
 
-let empCheck = Math.floor(Math.random() * 10) % 2;
-if (empCheck === 0) {
-    console.log("Employee is Absent");
+let empCheck = Math.floor(Math.random() * 10) % 3;
+let empWage =0;
+switch(empCheck){
+    case 1:
+        empWage = PART_TIME_HOURS*WAGE_PER_HOUR;
+        break;
+    case 2:
+        empWage = FULL_TIME_HOURS*WAGE_PER_HOUR;
+        break;
+    default:
+        empWage = 0;
+        break;
 }
-else{
-    console.log("Employee is Present");
-}
+console.log('Employee Wage is : '+ empWage);
