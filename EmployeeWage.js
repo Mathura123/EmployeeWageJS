2a779b5d37604e52d96a93e2ae1d45bde577df91
Mathura <mathuradas333@gmail.com>
2020-11-21 20:33:44 +0530

    [Mathura]Refractor. Use of Daily wage Array to get Total Wage. UC7.A

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,19 +1,30 @@
-const IS_PART_TIME = 1;
-const IS_FULL_TIME = 2;
-const PART_TIME_HOURS = 4;
-const FULL_TIME_HOURS = 8;
-const WAGE_PER_HOUR = 20;
-const WORKING_DAYS = 20;
-const MAX_WORK_HOURS = 160;
+// const IS_PART_TIME = 1;
+// const IS_FULL_TIME = 2;
+// const PART_TIME_HOURS = 4;
+// const FULL_TIME_HOURS = 8;
+// const WAGE_PER_HOUR = 20;
+// const WORKING_DAYS = 20;
+// const MAX_WORK_HOURS = 160;
+
+let EmployeeWage = {
+    IS_PARTTIME: 1,
+    IS_FULLTIME: 2,
+    PART_TIME_HOURS: 4,
+    FULL_TIME_HOURS: 8,
+    WAGE_PER_HOUR: 20,
+    MAX_WORK_HRS: 160,
+    MAX_WORK_DAYS: 20,
+    empDailyWageArray: new Array(),
+};
 
 function GetWorkHours(empCheck) {
     let workHours = 0;
     switch (empCheck) {
-        case 1:
-            workHours = PART_TIME_HOURS;
+        case EmployeeWage.IS_PARTTIME:
+            workHours = EmployeeWage.PART_TIME_HOURS;
             break;
-        case 2:
-            workHours = FULL_TIME_HOURS;
+        case EmployeeWage.IS_FULLTIME:
+            workHours = EmployeeWage.FULL_TIME_HOURS;
             break;
         default:
             workHours = 0;
@@ -23,22 +34,26 @@ function GetWorkHours(empCheck) {
 }
 
 function CalEmpWage(hrs) {
-    return hrs * WAGE_PER_HOUR;
+    return hrs * EmployeeWage.WAGE_PER_HOUR;
 }
 
 let totalWorkHours = 0;
-let dailyWageArr = new Array();
-let totalWorkDays =0;
-for (; totalWorkDays < WORKING_DAYS && totalWorkHours <= MAX_WORK_HOURS; totalWorkDays++) {
+let totalWorkDays = 0;
+for (; totalWorkDays < EmployeeWage.MAX_WORK_DAYS && totalWorkHours <= EmployeeWage.MAX_WORK_HRS; totalWorkDays++) {
     let empCheck = Math.floor(Math.random() * 10) % 3;
     let workHours = GetWorkHours(empCheck);
     totalWorkHours += workHours;
-    dailyWageArr.push(CalEmpWage(workHours));
+    if (totalWorkHours > EmployeeWage.MAX_WORK_HRS) {
+        workHours -= totalWorkHours - EmployeeWage.MAX_WORK_HRS;
+        totalWorkHours = EmployeeWage.MAX_WORK_HRS;
+    }
+    EmployeeWage.empDailyWageArray.push(CalEmpWage(workHours));
 }
-totalWorkHours = Math.min(totalWorkHours, MAX_WORK_HOURS);
 
 let empWage = CalEmpWage(totalWorkHours)
-console.log("EmpWage array : " + dailyWageArr.toString());
-console.log('Total Working days : '+ totalWorkDays)
-console.log('Total Employee wage : ' + empWage);
-console.log("Total Work hours : "+ totalWorkHours);
+let totalWage =0;
+EmployeeWage.empDailyWageArray.forEach((dailyWage)=>{totalWage += dailyWage});
+console.log("EmpWage array : " + EmployeeWage.empDailyWageArray.toString());
+console.log('Total Working days : ' + totalWorkDays)
+console.log('Total Employee wage : ' + totalWage);
+console.log("Total Work hours : " + totalWorkHours);
